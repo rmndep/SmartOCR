6e8cb49224d74124d8843948f784d47bad2a716e
Ramandeep Singh <aman@Amans-MacBook-Pro-2.local>
2026-01-23 02:25:32 +0530

    feat: phase 1 django setup with file upload system

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+def upload_file(request):
+    if request.method == "POST":
+        uploaded_file = request.FILES['file']
+
+        file_path = f"media/{uploaded_file.name}"
+
+        with open(file_path, 'wb+') as destination:
+            for chunk in uploaded_file.chunks():
+                destination.write(chunk)
+
+        return render(request, 'result.html', {
+            'file_name': uploaded_file.name,
+            'file_path': file_path
+        })
+
+    return render(request, 'upload.html')
